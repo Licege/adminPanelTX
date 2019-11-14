c36a5ce1adef9c8df06447145e6cafa5ea6f107b
Mark Turgunbaev <gotica-2@mail.ru>
2019-11-15 01:25:56 +0200

    dev

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -11,6 +11,20 @@ const Users = (props) => {
                 </div>
             </div>
             <div style={styles.Page}>
+                <div className={'card filter'}>
+                    <div className={'card-body filter-container'}>
+                        <span className={'filter-header'}>Фильтры</span>
+                        <div className={'filter-main'}>
+                            <input type={'text'} placeholder={'Фамилия'} className={'filter-main-input -name form-control'} />
+                            <input type={'text'} placeholder={'Имя'} className={'filter-main-input -name form-control'} />
+                        </div>
+                        <div className={'filter-actions'}>
+                            <span className={'filter-actions-reset'}>Сбросить</span>
+                            <span className={'filter-actions-apply'}>Сбросить</span>
+                        </div>
+                    </div>
+                </div>
+
                 <div style={styles.Card}>
                     <Table responsive>
                         <thead className={'table-thread'}>
